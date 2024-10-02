1217ab668902263d620d71514cb94ce5de15a0b5
nap-pac <napoleonpaciente@yahoo.com>
2024-10-02 17:53:53 +0800

    Changed names to relevant

---
+++ IsoForMode.py
@@ -0,0 +1,8 @@
+#Libraries
+import pandas as pd
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+#rest is need for AI
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
