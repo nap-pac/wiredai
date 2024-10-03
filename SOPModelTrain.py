34f73f1d62a70c7fa91c9bdb10cbbaa09ebb90fc
nap-pac <napoleonpaciente@yahoo.com>
2024-10-03 16:49:58 +0800

    Added SOM model

---
+++ SOPModelTrain.py
@@ -0,0 +1,27 @@
+# Libraries
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import pickle
+from sklearn.preprocessing import MinMaxScaler
+from minisom import MiniSom
+
+# Import data
+fileName = 'extracted_features.csv'
+data = pd.read_csv(fileName)
+
+# Select Features
+features = data.iloc[:, 1].values
+scaler = MinMaxScaler()
+normalizedData = scaler.fit_transform(features)
+
+# Create SOM Model
+som_size = 100
+som = MiniSom(som_size, som_size, normalizedData.shape[1], sigma=1.0, learning_rate=0.5)
+
+# Train model
+som.train(normalizedData, num_iteration=1000)
+
+# Save the model
+with open('SOMmodel.pk1', 'wb') as f:
+    pickle.dump(som, f)
